6e172e414f673847633019e78062019ebc38f239
Heqiao Ruan <hruan@ucdavis.edu>
2021-01-16 21:46:28 -0800

    Update 1571.sql

--- 1571.sql
+++ 1571.sql
@@ -2,3 +2,5 @@
 SELECT W.name AS warehouse_name, SUM(P.Width * P.Length * P.Height * W.units) AS volume
 FROM Warehouse AS w LEFT JOIN Products AS P ON W.product_id = P.product_id 
 GROUP BY W.name;
+
+#IFNULL(,0 )
