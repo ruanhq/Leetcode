77e87ed1e2f43ae1a55172592978ac04d47680c3
Heqiao Ruan <hruan@ucdavis.edu>
2021-01-16 22:00:14 -0800

    U

---
+++ 1126.sql
@@ -0,0 +1,15 @@
+#1126:Active business:
+#
+WITH T1 AS
+(
+SELECT event_type, avg(occurences) AS AVG_OCCUR
+FROM Events GROUP BY event_type
+)
+SELECT EE.business_id
+FROM events AS EE
+JOIN T1 
+ON 
+EE.event_type = T1.event_type
+AND EE.occurences > T1.AVG_OCCUR
+GROUP BY EE.business_id
+HAVING COUNT(*) > 1;
--- 1571.sql
+++ 1571.sql
@@ -3,4 +3,3 @@ SELECT W.name AS warehouse_name, SUM(P.Width * P.Length * P.Height * W.units) AS
 FROM Warehouse AS w LEFT JOIN Products AS P ON W.product_id = P.product_id 
 GROUP BY W.name;
 
-#IFNULL(,0 )
