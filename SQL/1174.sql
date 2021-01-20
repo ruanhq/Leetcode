d2e5d73794e034514ce08980b50de256d42f0d80
Heqiao Ruan <hruan@ucdavis.edu>
2021-01-20 12:17:29 -0800

    update-0120

--- SQL/1174.sql
+++ SQL/1174.sql
@@ -1,11 +1,9 @@
 #1174: Immediate Food Delivery II
 #
-WITH A1 AS(
-SELECT delivery_id, customer_id, order_date, customer_pref_delivery_date,
-    RANK() OVER (PARTITION BY customer_id ORDER BY order_date) AS rk
+WITH A1 AS (
+SELECT *, RANK() OVER (PARTITION BY customer_id ORDER BY order_date) AS rk
 FROM Delivery
-)
-SELECT ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date
-                THEN 1 ELSE 0 END) * 100, 2) AS immediate_percentage
-                FROM A1
-                WHERE rk = 1;
+) 
+
+SELECT ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100, 2) AS immediate_percentage
+FROM A1 WHERE rk = 1;
