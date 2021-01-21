d10bb173a55a4283451684593d57f08b41d1d747
Heqiao Ruan <hruan@ucdavis.edu>
2021-01-20 23:27:27 -0800

    Create 1264.sql

---
+++ SQL/1264.sql
@@ -0,0 +1,11 @@
+#1264: Page Recommendation
+
+SELECT DISTINCT page_id AS recommended_page
+FROM Likes
+WHERE user_id IN (
+SELECT user1_id FROM Friendship WHERE user2_id = 1
+UNION 
+SELECT user2_id FROM Friendship WHERE user1_id = 1
+) AND page_id NOT IN (
+SELECT page_id FROM Likes WHERE user_id = 1
+)
