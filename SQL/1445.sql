f46e3ad1b99b25c9ebdecf7c704658350735a036
Heqiao Ruan <hruan@ucdavis.edu>
2021-01-21 20:33:45 -0800

    Create 1445.sql

---
+++ SQL/1445.sql
@@ -0,0 +1,7 @@
+#1445. Apples & Oranges
+SELECT sale_date, SUM(IF(fruit = 'apples', sold_num, -sold_num)) AS diff
+FROM Sales
+GROUP BY sale_date;
+
+#SUM(IF(fruit = 'apples', sold_num, - sold_num)) AS diff
+#FROM Sales GROUP BY sale_date;
