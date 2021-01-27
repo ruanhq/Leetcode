0fcc411aea9f5d898a303cec34087f5b1b26cd64
Heqiao Ruan <hruan@ucdavis.edu>
2021-01-26 16:03:27 -0800

    Update 1204.sql

--- SQL/1204.sql
+++ SQL/1204.sql
@@ -1,1 +1,8 @@
 #1204. Last Person to Fit in the elevator
+SELECT person_name
+FROM 
+(SELECT person_name, SUM(weight) OVER (ORDER BY turn ASC) AS running_total
+FROM Queue) A1
+WHERe A1.running_total <= 1000
+ORDER BY running_total DESC
+LIMIT 1
