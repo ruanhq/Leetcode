e6540bb875da9d04c87daf6438ecd46a6962c41b
Heqiao Ruan <hruan@ucdavis.edu>
2021-01-26 16:45:01 -0800

    Update 1613.sql

--- SQL/1613.sql
+++ SQL/1613.sql
@@ -1,1 +1,8 @@
-#1613. Find the Missing IDs
+#1613. Find missing ids in consecutive sequence.
+
+WITH RECURSIVE seq AS (
+    SELECT 1 AS value UNION ALL SELECT value + 1 FROM seq WHERE value < (SELECT MAX(customer_id) FROM Customers))
+SELECT DISTINCT s.value AS ids
+FROM seq s, Customers c
+WHERE s.value not in (SELECT customer_id FROM Customers)
+ORDER BY 1 ASC;
