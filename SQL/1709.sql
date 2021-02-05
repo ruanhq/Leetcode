cf1b841364625afaa0a05b884e3704db28245137
Heqiao Ruan <hruan@ucdavis.edu>
2021-02-05 12:24:29 -0800

    Create 1709.sql

---
+++ SQL/1709.sql
@@ -0,0 +1,8 @@
+#1709. Biggest Window Between Visits
+SELECT user_id, MAX(diff) AS biggest_window
+FROM (
+SELECT user_id, DATEDIFF(LEAD(visit_date, 1, '2021-01-01') OVER (PARTITION BY user_id ORDER BY visit_date), visit_date) AS 
+    diff FROM UserVisits
+) AS A
+GROUP BY user_id
+ORDER BY user_id;
