98985c8faa9e050b2fa38ac56ebfd74b8621bd97
Heqiao Ruan <hruan@ucdavis.edu>
2021-02-22 19:45:17 -0800

    Update 14.py

--- Algorithm/14.py
+++ Algorithm/14.py
@@ -1,1 +1,11 @@
-#14. Longest Common Prefix
+#14. Longest Common Prefix:
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        zips = list(zip(*strs))
+        pre_fix = ""
+        for i in zips:
+            if len(set(i)) == 1:
+                pre_fix += i[0]
+            else:
+                break
+        return pre_fix
