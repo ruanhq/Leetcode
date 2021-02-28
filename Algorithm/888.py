bea8e813f0dde1af7815e7258bd4ec6314b0c806
Heqiao Ruan <hruan@ucdavis.edu>
2021-02-27 20:19:01 -0800

    update

--- Algorithm/888.py
+++ Algorithm/888.py
@@ -1,13 +1,10 @@
 #888. Fair Candy Swap
-class Solution(object):
-    def fairCandySwap(self, A, B):
-        Sa, Sb = sum(A), sum(B)
-        setB = set(B)
-        for x in A:
-            if x + (Sb - Sa) / 2 in setB:
-                return [x, x + (Sb - Sa) / 2]
 def fairCandySwap(self, A: List[int], B: List[int]) -> List[int]:
 	nA = sum(A)
 	nB = sum(B)
+	setB = set(B)
+	for a in A:
+		if a + (nB - nA)/2 in setB:
+			return [a, a + (nB -nA)/2]
 
 
