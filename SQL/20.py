5e57e567894b81c6bc95b0705227075016c3f956
Heqiao Ruan <hruan@ucdavis.edu>
2021-03-04 15:41:49 -0800

    Create 20.py

---
+++ SQL/20.py
@@ -0,0 +1,14 @@
+#valid parenthesis: a typical first in first out structure.
+class Solution:
+  def isValid(self, s: str) -> bool:
+  	stack = []
+  	start_par = ["(", "[", "{"]
+  	maps = {'(': ')', '[': ']', '{': '}'}
+  	for i in range(len(s)):
+  		if s[i] in start_par:
+  			stack.append(s[i])
+  		if stack and maps[stack[-1]] == s[i]:
+  			stack.pop()
+  		else:
+  			False
+  	return not stack
