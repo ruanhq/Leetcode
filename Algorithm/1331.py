7f9976f9846ed43f2599d2cba3f92ed653f90e6b
Heqiao Ruan <hruan@ucdavis.edu>
2021-03-19 23:36:54 -0700

    Create 1331.py

---
+++ Algorithm/1331.py
@@ -0,0 +1,12 @@
+#1331. Rank Transform of an array:
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+    	hashSet = {}
+    	rank = 1
+    	for num in sorted(arr):
+    		if num not in hashSet:
+    			hashSet[num] = rank 
+    			rank += 1
+    	for idx, val in enumerate(arr):
+    		arr[idx] = hashSet[val]
+    	return arr
