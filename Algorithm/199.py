5cf0ad662e5ebc868e93f892e2e2c131fac5dc7e
Heqiao Ruan <hruan@ucdavis.edu>
2021-04-05 13:01:16 -0700

    Update 199.py

--- Algorithm/199.py
+++ Algorithm/199.py
@@ -18,3 +18,38 @@ class Solution(object):
             result.append(currentNode.val)
         return result
 
+from collections import deque
+
+matrix = [[4,5,6], [7,8,9]]
+for row in matrix:
+    row.pop()
+
+
+def spiralOrder(matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: List[int]
+    """
+    n = len(matrix)
+    result = []
+    while matrix:
+        result += matrix.pop(0)
+        if matrix and matrix[0]:
+            for row in matrix:
+                result.append(row.pop())
+        if matrix and matrix[0] != "[]":
+            result += matrix.pop()[::-1]
+        if matrix and matrix[0]:
+            for row in matrix[::-1]:
+                result.append(row.pop(0))
+    return result
+
+
+matrix = [[1],[2],[3]]
+spiralOrder(matri)
+
+
+
+
+
+
