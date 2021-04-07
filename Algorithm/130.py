9833c26f1209261fef75ccd0f7474068d283c2e6
Heqiao Ruan <hruan@ucdavis.edu>
2021-04-06 22:36:17 -0700

    update

--- Algorithm/130.py
+++ Algorithm/130.py
@@ -1,32 +1,31 @@
 #130. Surrounding Islands
+#starting from the edge:
+class Solution:
+    def searching(self, matrix, row, col):
+        if 0 <= row < len(matrix[0]) and 0 <= col < len(matrix) and matrix[row][col] == "O":
+            matrix[row][col] == "DUIGOU"
+            self.searching(matrix, row - 1, col)
+            self.searching(matrix, row + 1, col)
+            self.searching(matrix, row, col - 1)
+            self.searching(matrix, row, col + 1)
 
-class Solution(object):
-    def searching(self, board, row, col):
-        if 0 <= row < len(board) and 0 <= col < len(board[0]) and board[row][col] == "O":
-            board[row][col] = "DUIGOU"
-            self.searching(board, row + 1, col)
-            self.searching(board, row - 1, col)
-            self.searching(board, row, col - 1)
-            self.searching(board, row, col + 1)
 
-    def solve1(self, board):
-        if not board or not board[0]:
-            return
-        for i in range(len(board[0])):
-            self.searching(board, 0,i)
-            self.searching(board, len(board) - 1, i)
-        for j in range(len(board)):
-            self.searching(board, j, 0)
-            self.searching(board, j, len(board[0]) - 1)
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == "DUIGOU":
-                    board[i][j] = "O"
+    def findSurroundIslands(self, matrix: List[List[int]]):
+        m = len(matrix)
+        n = len(matrix[0])
+        for i in range(m):
+            self.searching(matrix, 0, i)
+            self.searching(matrix, n - 1, i)
+        for i in range(n):
+            self.searching(matrix, i, 0)
+            self.searching(matrix, i, m - 1)
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == "DUIGOU":
+                    matrix[i][j] = "O"
                 else:
-                    board[i][j] = "X"
-        return board
-
-
+                    matrix[i][j] = "X"
+        return matrix
 
 
 
--- Algorithm/48.py
+++ Algorithm/48.py
@@ -15,3 +15,6 @@ class Solution:
 
         return matrix
 
+
+class Solution:
+    
